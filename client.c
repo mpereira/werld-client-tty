4180a73646cbd0af68b36a7307f50e1ae270272d
Murilo Pereira <murilo@murilopereira.com>
2011-11-02 15:50:15 -0200

    Cleanup.

--- client.c
+++ client.c
@@ -1,14 +1,10 @@
-#define _GNU_SOURCE
-#include <arpa/inet.h>
 #include <errno.h>
-#include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
 #include <unistd.h>
 
 #include "client.h"
