d1e6b81883ec5c28ae8b9810990c9b3b5dcd1c95
Murilo Pereira <murilo@murilopereira.com>
2011-11-05 22:34:34 -0200

    Initial implementation of the player_message_list module.

---
+++ player_message_list.h
@@ -0,0 +1,18 @@
+#ifndef WERLD_PLAYER_MESSAGE_LIST_H
+#define WERLD_PLAYER_MESSAGE_LIST_H
+
+struct player_message_list {
+  int player_id;
+  struct message_list *message_list;
+  struct player_message_list *next;
+};
+
+extern struct player_message_list *player_message_list;
+
+void player_message_list_malloc(struct player_message_list **);
+void player_message_list_init(struct player_message_list **);
+int player_message_list_add(struct player_message_list **, const char *, int);
+int player_message_list_remove(struct player_message_list **, const char *, int);
+void player_message_list_free(struct player_message_list *);
+
+#endif
