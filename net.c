cab5fd8b12e50a5fa43721985661cb4404a6902c
Murilo Pereira <murilo@murilopereira.com>
2011-12-04 23:05:19 -0200

    Block until all data is received.

--- net.c
+++ net.c
@@ -2,21 +2,22 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <sys/socket.h>
 
 static ssize_t
 net_exchange(int fd,
              void *buffer,
              size_t nbyte,
-             ssize_t (*function)(int fd, void *buffer, size_t nbyte)) {
+             int flags,
+             ssize_t (*function)(int fd, void *buffer, size_t nbyte, int flags)) {
   ssize_t bytes_exchanged;
   ssize_t total_bytes_exchanged;
 
   total_bytes_exchanged = 0;
 
   while (total_bytes_exchanged < (ssize_t) nbyte) {
-    if ((bytes_exchanged = (*function)(fd, buffer, nbyte)) < 0) {
-      perror((function == read) ? "read" : "write");
+    if ((bytes_exchanged = (*function)(fd, buffer, nbyte, flags)) < 0) {
+      perror((function == recv) ? "recv" : "send");
       exit(errno);
     }
     total_bytes_exchanged += bytes_exchanged;
@@ -29,9 +30,12 @@ ssize_t net_write(int fd, const void *buffer, size_t nbyte) {
   return(net_exchange(fd,
                       (void *) buffer,
                       nbyte,
-                      (ssize_t (*)(int, void *, size_t)) write));
+                      0,
+                      (ssize_t (*)(int, void *, size_t, int)) send));
 }
 
 ssize_t net_read(int fd, void *buffer, size_t nbyte) {
-  return(net_exchange(fd, buffer, nbyte, read));
+  int flags;
+  flags = MSG_WAITALL;
+  return(net_exchange(fd, buffer, nbyte, flags, recv));
 }
