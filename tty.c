a50c7514195cefd884bb650a716cc0e6999692f8
Murilo Pereira <murilo@murilopereira.com>
2011-12-11 12:47:26 -0200

    We don't need that many rows.

--- tty.c
+++ tty.c
@@ -3,10 +3,10 @@
 #include "ui.h"
 #include "werld_client.h"
 
-static const int WERLD_MIN_TERM_LINES = 25;
+static const int WERLD_MIN_TERM_LINES = 24;
 static const int WERLD_MIN_TERM_COLS = 80;
 const char *WERLD_SMALL_TERM_MSG = "Please increase your terminal size to at "
-                                   "least 80x25 or press q to quit.";
+                                   "least 80x24 or press q to quit.";
 
 bool tty_term_size_ok() {
   int lines, columns;
